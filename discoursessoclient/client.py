db4a85616f0f996d0bf3b066050d314e37706792
Tom Smyth <tom@sassafras.coop>
2019-04-19 23:16:04 -0400

    Add error cases for login

--- discoursessoclient/client.py
+++ discoursessoclient/client.py
@@ -31,8 +31,48 @@ class DiscourseSsoClientMiddleware:
         to_url = f'{settings.SSO_PROVIDER_URL}?sso={payload}&sig={signature}'
         return HttpResponseRedirect(to_url)
 
+    def sso_login(self, request):
+        # Check signature
+        payload = request.GET.get('sso')
+        signature = request.GET.get('sig')
+
+        if payload is None or signature is None:
+            return HttpResponseBadRequest('no_payload_or_sig')
+
+        payload = urllib.parse.unquote(payload)
+        if len(payload) == 0:
+            return HttpResponseBadRequest('empty_payload')
+
+        if 'sso_nonce' not in request.session:
+            return HttpResponseBadRequest('no_nonce_in_session')
+
+        nonce = request.session['sso_nonce']
+        try:
+            decoded = str(base64.decodestring(bytes(payload, encoding='utf-8')))
+            if nonce not in decoded:
+                return HttpResponseBadRequest('wrong_nonce_in_payload')
+        except ValueError:
+            return HttpResponseBadRequest('bad_payload_encoding')
+
+        if not hmac.compare_digest(self.sign_payload(payload), signature):
+            return HttpResponseBadRequest('invalid_signature')
+
+        parse_qs(payload, strict_parsing=True)
+
+        return HttpResponseBadRequest('good_so_far')
+
     # Generates signature for utf-8 string payload.
     def sign_payload(self, payload):
         return hmac.new(bytes(settings.SSO_SECRET, encoding='utf-8'),
                         bytes(payload, encoding='utf-8'),
                         digestmod=hashlib.sha256).hexdigest()
+
+
+    def get_user(self, payload):
+        # If existing sso record for external_id, update and return associated user
+        # Else, if user with matching email, update them, create sso record, and return
+        # Else, create user and sso record
+        1
+
+    def update_user_from_sso_payload(self, user, payload):
+        1
--- discoursessoclient/tests/test_client.py
+++ discoursessoclient/tests/test_client.py
@@ -1,3 +1,4 @@
+import base64
 from django.test import TestCase
 from unittest.mock import Mock, patch
 
@@ -22,3 +23,47 @@ class SsoInitTestCase(TestCase):
                 'mcmV0dXJuX3Nzb191cmw9aHR0cHM6Ly9sb2NhbGhvc3Q6ODAwMC9sb2'
                 'dpbg%3D%3D%27&sig=94f4e09bf34e7003acc93da3b0182306d64de'
                 'cd2ca4c295953f93f286e40f6f3')
+
+class SsoLoginTestCase(TestCase):
+
+    def setUp(self):
+        self.middleware = DiscourseSsoClientMiddleware(lambda x: x)
+
+    def test_with_no_payload(self):
+        def asserts(response):
+            self.assertEqual(response.content, b'no_payload_or_sig')
+        self.call_middleware({}, {}, asserts)
+
+    def test_with_empty_payload(self):
+        def asserts(response):
+            self.assertEqual(response.content, b'empty_payload')
+        self.call_middleware({'sso': '', 'sig': ''}, {}, asserts)
+
+    def test_with_no_nonce_in_session(self):
+        def asserts(response):
+            self.assertEqual(response.content, b'no_nonce_in_session')
+        self.call_middleware({'sso': 'x', 'sig': ''}, {}, asserts)
+
+    def test_with_bad_payload(self):
+        def asserts(response):
+            self.assertEqual(response.content, b'bad_payload_encoding')
+        self.call_middleware({'sso': 'x', 'sig': ''}, {'sso_nonce': 'y'}, asserts)
+
+    def test_with_wrong_nonce(self):
+        def asserts(response):
+            self.assertEqual(response.content, b'wrong_nonce_in_payload')
+        qs = {'sso': base64.b64encode(b'sso_nonce=31ab53').decode(encoding='utf-8'), 'sig': ''}
+        self.call_middleware(qs, {'sso_nonce': '31ab54'}, asserts)
+
+    def test_with_bad_signature(self):
+        def asserts(response):
+            self.assertEqual(response.content, b'invalid_signature')
+        qs = {'sso': base64.b64encode(b'sso_nonce=31ab53').decode(encoding='utf-8'), 'sig': 'asfadfas'}
+        self.call_middleware(qs, {'sso_nonce': '31ab53'}, asserts)
+
+    def call_middleware(self, qs, session, func):
+        request = Mock(path="/sso/login", session=session, GET=Mock(get=lambda x: qs[x] if x in qs else None))
+        with self.settings(SSO_PROVIDER_URL='https://example.com/sso',
+                           SSO_SECRET='b54cc7b3e42b215d1792c300487f1cb1'):
+            response = self.middleware.__call__(request)
+            func(response)
