84bd88d30ecb5025b7a1286b3f2cd5c2dad9fa87
Tom Smyth <tom@sassafras.coop>
2019-04-26 20:46:33 -0400

    Add comments

--- models.py
+++ models.py
@@ -2,8 +2,13 @@ from django.db import models
 from django.contrib.auth.models import User
 
 
+# Models a record of an SSO sign in.
 class SsoRecord(models.Model):
+    # The ID of the user on the SSO provider system.
     external_id = models.TextField(primary_key=True)
-    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    # This flag can be used to sign the user out upon the next request.
     sso_logged_in = models.BooleanField(default=False)
+
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
     created_on = models.DateTimeField(auto_now_add=True)
